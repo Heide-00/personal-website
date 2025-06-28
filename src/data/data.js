a690bba4e790afc210f8bce6f132eb5dc687162b
Heide-00 <gulaslan93@gmail.com>
2025-06-28 14:33:12 +0300

    data.js
    İçerik oluşturuldu.

---
+++ src/data/data.js
@@ -0,0 +1,155 @@
+//Proje içeriği oluşturuldu.
+export const content = {
+  hero: {
+    tr: {
+      title: "almila",
+      subhead: "Ben bir Frontend geliştiricisiyim...",
+      explanation:
+        "...harika kullanıcı deneyimleri ile sağlam ve ölçeklenebilir ön uç ürünleri oluşturmayı seven biriyim.",
+      buttons: {
+        github: "Github",
+        linkedln: "Linkedln"
+      }
+    },
+    en: {
+      title: "almila",
+      subhead: "I am a Frontend Developer...",
+      explanation:
+        "...who likes to craft solid and scalable frontend products with great user experiences",
+      buttons: {
+        github: "Github",
+        linkedln: "Linkedln"
+      }
+    }
+  },
+
+  flows: {
+    tr: {
+      title: "Yetenekler",
+      items: [
+        { title: "JAVASCRIPT", icon: "js-logo 1.png", alt: "JavaScript logosu" },
+        { title: "REACT", icon: "Rectangle 35.png", alt: "React logosu" },
+        { title: "REDUX", icon: "Group 101 (1).png", alt: "Redux logosu" },
+        { title: "NODE", icon: "node-logo 1.png", alt: "NodeJS logosu" },
+        { title: "VS CODE", icon: "Group 101.png", alt: "VS Code logosu" },
+        { title: "FİGMA", icon: "figma-logo 1.png", alt: "Figma logosu" }
+      ]
+    },
+    en: {
+      title: "Skills",
+      items: [
+        { title: "JAVASCRIPT", icon: "js-logo 1.png", alt: "JavaScript logo" },
+        { title: "REACT", icon: "Rectangle 35.png", alt: "React logo" },
+        { title: "REDUX", icon: "Group 101 (1).png", alt: "Redux logo" },
+        { title: "NODE", icon: "node-logo 1.png", alt: "NodeJS logo" },
+        { title: "VS CODE", icon: "Group 101.png", alt: "VS Code logo" },
+        { title: "FİGMA", icon: "figma-logo 1.png", alt: "Figma logo" }
+      ]
+    }
+  },
+
+  profile: {
+    tr: {
+      leftText: {
+        title:"Profil",
+        subheading: "Temel Bilgiler",
+        birthdate: "24.03.1996",
+        cityresidence: "Ankara",
+        education: "Hacettepe Ünv. Biyoloji",
+        preference: "Frontend, UI"
+      },
+      rightText: {
+        title: "Hakkımda",
+        paragraphone:
+          "Lorem ipsum, dolar sit amet consectetur adipisicing elit. Veniam aut, odit laborum aliquam voluptatum nisi mollitia.",
+        paragraphtwo:
+          "Minima accusamus ratione soluta aperiam sit voluptate? Dicta quod deserunt quam temporibus cumque magnam!"
+      }
+    },
+    en: {
+      leftText: {
+        title:"Profile",
+        subheading:"Basic Information",
+        birthdate: "24.03.1996",
+        cityresidence: "Ankara",
+        education: "Hacettepe Ünv. Biology",
+        preference: "Frontend, UI"
+      },
+      rightText: {
+        title: "About Me",
+        paragraphone:
+          "Lorem ipsum, dolar sit amet consectetur adipisicing elit. Veniam aut, odit laborum aliquam voluptatum nisi mollitia.",
+        paragraphtwo:
+          "Minima accusamus ratione soluta aperiam sit voluptate? Dicta what deserunt quam temporibus cumque magnam!"
+      }
+    }
+  },
+
+  projects: {
+    tr: {
+      title: "Projeler",
+      items: [
+        {
+          image: "/assets/projects/rectangle.png",
+          subhead: "Workintech",
+          paragraph:
+            "Kullanıcılarınızın hangi çerezleri kabul edip etmeyeceğini seçmelerine olanak tanıyan basit, özelleştirilebilir, minimum kurulumlu bir çerez eklentisidir. Bu eklenti vanilla JS, SCSS ve Parcel Bundler ile oluşturulmuştur ve bir NPM paketi olarak mevcuttur ve git deposu kod ve temalarda her türlü özelleştirmeyi mümkün kılar.",
+          buttons: { react: "react", redux: "redux", vercel: "vercel" },
+          underlinedtextone: "Siteyi Görüntüle",
+          underlinedtexttwo: "Github"
+        },
+        {
+          image: "/assets/projects/rectangle-41.png",
+          subhead: "Journey",
+          paragraph:
+            "Kullanıcılarınızın hangi çerezleri kabul edip etmeyeceğini seçmelerine olanak tanıyan basit, özelleştirilebilir, minimum kurulumlu bir çerez eklentisidir. Bu eklenti vanilla JS, SCSS ve Parcel Bundler ile oluşturulmuştur ve bir NPM paketi olarak mevcuttur ve git deposu kod ve temalarda her türlü özelleştirmeyi mümkün kılar.",
+          buttons: { react: "react", redux: "redux", vercel: "vercel" },
+          underlinedtextone: "Siteyi Görüntüle",
+          underlinedtexttwo: "Github"
+        }
+      ]
+    },
+    en: {
+      title: "Projects",
+      items: [
+        {
+          image: "/projects/rectangle.png",
+          subhead: "Workintech",
+          paragraph:
+            "A simple,customizable,minimal setup cookie plugin that allows your users to select which cookies to accept or decline.This was created with vanilla JS,SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.",
+          buttons: { react: "react", redux: "redux", vercel: "vercel" },
+          underlinedtextone: "View Site",
+          underlinedtexttwo: "Github"
+        },
+        {
+          image: "/projects/rectangle-41.png",
+          subhead: "Journey",
+          paragraph:
+            "A simple,customizable,minimal setup cookie plugin that allows your users to select which cookies to accept or decline.This was created with vanilla JS,SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.",
+          buttons: { react: "react", redux: "redux", vercel: "vercel" },
+          underlinedtextone: "View Site",
+          underlinedtexttwo: "Github"
+        }
+      ]
+    }
+  },
+
+  footer: {
+    tr: {
+      title: "Bana bir mesaj gönder!",
+      explanation:
+        "Bir sorunuz veya öneriniz mi var, ya da sadece merhaba demek mi istiyorsunuz? Hadi gelin.",
+      underlinedtext: "almilasucode@gmail.com"
+    },
+    en: {
+      title: "Send me a message!",
+      explanation: "Got a question or proposal, or just want to say hello? Go ahead.",
+      underlinedtext: "almilasucode@gmail.com"
+    }
+  }
+};
+
+export default content;
+
+      
+    
